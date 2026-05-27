415dc2a3b12b4784773be8bfdc663549ba9e5235
zzy <2218847957@qq.com>
2026-05-27 10:11:16 +0800

    Initial Commit

---
+++ Sarty/SartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SartyApp.swift
+//  Sarty
+//
+//  Created by yangyang on 2026/5/27.
+//
+
+import SwiftUI
+
+@main
+struct SartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
